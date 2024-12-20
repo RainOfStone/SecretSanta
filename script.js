39f1745e3db29c7c1abd6f6be66708c3a58c8ff0
RainOfStone <RainOfStone>
2024-12-20 15:52:20 -0500

    finished Pineapple Bread added credits

--- script.js
+++ script.js
@@ -98,12 +98,12 @@ const PBText = document.createElement('div'),
 PB_Btn = document.createElement('button')
 PB_Btn.className = 'pbBtn'
 PBText.className = 'pbText'
-PBText.style.cssText = 'display: flex; flex-direction: row; height: 260px; width 180px; justify-content: center; align-items: center; font-size: 18px; border: 1px solid black;'
+PBText.style.cssText = 'display: flex; flex-direction: row; height: 260px; width 180px; justify-content: center; align-items: center; font-size: 18px;'
 PBText.textContent = 'You sit on the hot sofa as the lights make you temporarily dizzy. The game of bravery: Pineapple Bread is approaching its final stages as everyone awaits your decision as the final survivor.'
 PB_Btn.style.cssText = 'height: 20px; width: 70px; border: 1px solid black;'
 PB_Btn.textContent = 'continue'
 function DisplayPB(){
-        LoginDiv.style.cssText = 'display: flex; flex-direction: row; border: 1px solid black; height: 300px; width: 480px; justify-content: center; align-items: center; font-size: 20px;'
+        LoginDiv.style.cssText = 'display: flex; flex-direction: row; height: 300px; width: 480px; justify-content: center; align-items: center; font-size: 20px;'
         LoginDiv.appendChild(PbPic)
         LoginDiv.appendChild(PBText)
         LoginDiv.appendChild(PB_Btn)
@@ -122,7 +122,7 @@ function PBTop(){
     DisplayPB()
 }
 else {
-    while (PBTop <= 3) {
+    while (PBTop <= 3 || count == 6) {
         const Pics = document.createElement('img')
         Pics.src = `candy/pb${PBTop}.png`
         Pics.style.height = '200px'
@@ -134,7 +134,7 @@ else {
 }
 function PBBottom(){
     let PBBottom = 4
-    while (PBBottom <= 6) {
+    while (PBBottom <= 6 || count == 6) {
         const Pics1 = document.createElement('img')
         Pics1.src = `candy/pb${PBBottom}.png`
         Pics1.style.height = '200px'
@@ -165,6 +165,33 @@ function audienceAdder() {
     }
 }
 if (count == 2) {
+    //PBTop()
+        console.log('random pineapples ran!')
+        let RandomPineapples = 7
+        while (RandomPineapples <= 9) {
+        const Pics2 = document.createElement('img')
+        Pics2.src = `candy/a${RandomPineapples}.png`
+        Pics2.style.height = '200px'
+        Pics2.style.width = '220px'
+        CandyTop.appendChild(Pics2)
+        RandomPineapples++
+        }
+    PBBottom()
+}
+if (count == 3) {
+    //PBTop()
+    let Plasma = 11
+    while (Plasma >= 10) {
+        const Pics3 = document.createElement('img')
+        Pics3.src = `candy/a${Plasma}.png`
+        Pics3.style.height = '200px'
+        Pics3.style.width = '330px'
+        CandyTop.appendChild(Pics3)
+        Plasma--
+    }
+    PBBottom()
+}
+if (count == 5) {
     PBTop()
     PBBottom()
 }
@@ -195,6 +222,7 @@ function PBTextChanger(){
     }
     if (count == 3) {
         PBText.textContent = 'Your brain freezes as images of the last runner-up being evaporated by a plasma pineapple rush through your mind.'
+        PB_imgDel()
     }
     if (count == 4) {
         PBText.textContent = '"Pineapple or bread!?" The spectators impatiently spur you on.'
@@ -210,6 +238,28 @@ function PBTextChanger(){
         Bread.appendChild(BreadBtn)
         LoginDiv.removeChild(PB_Btn)
     }
+    if (count == 6 && Choice == 'pineapple') {
+        PBText.style.fontSize = '24px'
+        PBText.textContent = `Ultimately, you have ${Results} gain a candy cane "pineapple".`
+        console.log('results:', Results, randomNum)
+    }
+    else if (count == 6 && Choice == 'bread') {
+        PBText.style.fontSize = '24px'
+        PBText.textContent = 'Ultimately, you give up. Still, the host gives you a candy cane for surviving the game.'
+    }
+    if (count == 7) {
+        PBText.textContent = 'Ho ho ho! Merry christmas Y/N! Here, have a candy cane: 🍬 + 🦯'
+        PB_Btn.textContent = 'credits'
+    }
+    if (count == 8) {
+        PBText.style.fontSize = '16px'
+        PBText.textContent = "Credits: Dialog writers: Hsr lore writers, Website Creator: Santa, Actors: Pineapple as pineapple, Bread as bread, Smiski lifting as .5 head, Matteo as disgusted young man, Happy Feet as yellow emoji with grippers on full display, Kaedee as Menacing young girl (Please don't show her this 🙏 she'll kill me 💀)"
+        PB_Btn.textContent = 'continue'
+    }
+    if (count == 9) {
+        PBText.textContent = 'Matteo x Caylin (🥰) as two people facing down, Julio as snachted baddie 😎, Rocks as rocks, Leaves as leaves, IX as blackhole, Shocked Black Guy as last runner-up, Adobe Plasma Gun as plasma gun and Y/N as final survivor. Thanks for going through the slides of this glorified powerpoint and Merry Christmas!'
+        LoginDiv.removeChild(PB_Btn)
+    }
 }
 const Pineapple = document.createElement('div'),
 PineappleBtn = document.createElement('button'),
@@ -223,5 +273,37 @@ Pineapple.textContent = 'Choose Pineapple with a chance to "win."'
 PineappleBtn.textContent = 'take a chance.'
 Bread.textContent = 'Choose Bread.'
 BreadBtn.textContent = 'Give up.'
-
+let Choice
+PineappleBtn.addEventListener('click', (event) => {
+    Choice = 'pineapple'
+    PBText.removeChild(Pineapple)
+    Pineapple.removeChild(PineappleBtn)
+    PBText.removeChild(Bread)
+    Bread.removeChild(BreadBtn)
+    PBText.style.flexDirection = 'row'
+    PBText.style.justifyContent = 'center'
+    PBText.textContent = `One doesn't get rich without taking risks! As you swallow the pineapple, a bitterness runs over your tongue.`
+    LoginDiv.appendChild(PB_Btn)
+    PB_imgDel()
+})
+BreadBtn.addEventListener('click', (event) => {
+    Choice = 'bread'
+    PBText.removeChild(Pineapple)
+    Pineapple.removeChild(PineappleBtn)
+    PBText.removeChild(Bread)
+    Bread.removeChild(BreadBtn)
+    PBText.style.flexDirection = 'row'
+    PBText.style.justifyContent = 'center'
+    PBText.textContent = `You become deeply aware of a great truth: That no matter how much money you have, you have to be alive to spend it.`
+    LoginDiv.appendChild(PB_Btn)
+    PB_imgDel()
+})
+let randomNum = Math.random(),
+Results
+if (randomNum <= 0.5) {
+    Results = 'lost yet'
+}
+else if (randomNum >= 0.5) {
+    Results = 'won and'
+}
 
